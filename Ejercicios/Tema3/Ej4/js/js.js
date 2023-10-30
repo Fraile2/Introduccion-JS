f61dff5981385a3e72845287eb1a305c75d1f369
Fraile2 <alejandrofraile421@gmail.com>
2023-10-30 12:36:07 +0100

    aa

---
+++ Ejercicios/Tema3/Ej4/js/js.js
@@ -0,0 +1,7 @@
+let valores = [true, 5, false, "Hola", "adios", 2];
+
+if (valores[3]<valores[4]) {
+    alert("El texto "+valores[4]+ " es mayor." );
+} else if (valores[3]>valores[4]) {
+    alert("El texto "+ valores[3]+ " es mayor.")
+}
