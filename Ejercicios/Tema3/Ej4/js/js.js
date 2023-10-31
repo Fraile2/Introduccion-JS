5cefd80b7704e704cd12b45ce0447735ee4bf49c
Fraile2 <alejandrofraile421@gmail.com>
2023-10-31 07:34:40 +0100

    Ej4 (Incompleto)
    Falta el apartado 3

--- Ejercicios/Tema3/Ej4/js/js.js
+++ Ejercicios/Tema3/Ej4/js/js.js
@@ -4,4 +4,13 @@ if (valores[3]<valores[4]) {
     alert("El texto "+valores[4]+ " es mayor." );
 } else if (valores[3]>valores[4]) {
     alert("El texto "+ valores[3]+ " es mayor.")
+} else {
+    alert("El texto "+ valores[3] +" y "+ valores[4] + " son iguales.")
+
+}
+
+if (valores[0]) {
+    alert("El valor contrario a "+ valores[0] +" es: "+ !valores[0])
+} else {
+    alert("El valor contrario a "+ valores[1] +" es: "+ !valores[1])
 }
