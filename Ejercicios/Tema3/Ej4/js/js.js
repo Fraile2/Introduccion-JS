fd5a676393ac0fabec775d8c8473ed0eaeb1bb25
Fraile2 <alejandrofraile421@gmail.com>
2023-10-31 07:45:15 +0100

    ejercicio 4 hecho

--- Ejercicios/Tema3/Ej4/js/js.js
+++ Ejercicios/Tema3/Ej4/js/js.js
@@ -1,5 +1,9 @@
 let valores = [true, 5, false, "Hola", "adios", 2];
 
+/*
+    Parte 1: Determinar cual de los elementos de texto es mayor.
+*/
+
 if (valores[3]<valores[4]) {
     alert("El texto "+valores[4]+ " es mayor." );
 } else if (valores[3]>valores[4]) {
@@ -8,9 +12,21 @@ if (valores[3]<valores[4]) {
     alert("El texto "+ valores[3] +" y "+ valores[4] + " son iguales.")
 
 }
+/*
+    Parte 2: Utilizando exclusivamente los dos valores booleanos del array
+*/
+alert(valores[0]);
+alert(valores[2]);
+/*
+    Parte 3: Determinar el resultado de 5 operaciones matemáticas realizadas con los 2 elementos numéricos
+*/
 
-if (valores[0]) {
-    alert("El valor contrario a "+ valores[0] +" es: "+ !valores[0])
-} else {
-    alert("El valor contrario a "+ valores[1] +" es: "+ !valores[1])
-}
+alert(valores[1]+" + "+valores[5]+" = "+(valores[1]+valores[5]));
+alert(valores[1]+" - "+valores[5]+" = "+(valores[1]-valores[5]));
+alert(valores[1]+" * "+valores[5]+" = "+(valores[1]*valores[5]));
+alert(valores[1]+" / "+valores[5]+" = "+(valores[1]/valores[5]));
+alert(valores[1]+" % "+valores[5]+" = "+(valores[1]%valores[5]));
+
+
+
+
