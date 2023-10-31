4c1798db79249fb541c0f5dd4ffdbe0476f35e50
Fraile2 <alejandrofraile421@gmail.com>
2023-10-31 11:25:59 +0100

    Ej5 acabado
    T3

---
+++ Ejercicios/Tema3/EJ5/js/js.js
@@ -0,0 +1,15 @@
+let numero1=5;
+let numero2=8;
+
+if (numero1<numero2) {
+    alert(numero1+" no es mayor que "+numero2);
+}
+if (numero2>0) {
+    alert(numero2+" es positivo.");
+}
+if (numero1<0) {
+    alert(numero1+ "es negativo o distinto de 0");
+}
+if (numero1+1<numero2) {
+    alert("Sumando 1 a "+ numero1 +" no le hace mayor que "+numero2);
+}
