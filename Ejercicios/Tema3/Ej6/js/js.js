2efab5475910a54022d034f599aa6ee4c28f5815
Fraile2 <alejandrofraile421@gmail.com>
2023-10-31 11:43:45 +0100

    Ej6 DNI
    Solucionado

--- Ejercicios/Tema3/Ej6/js/js.js
+++ Ejercicios/Tema3/Ej6/js/js.js
@@ -1,8 +1,3 @@
 let RESTOS=['T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E','T'];
-let dni="02759617";
-let dni_resto=parseInt(dni)%RESTOS.length();
-for (const r of RESTOS) {
-    if (dni_resto==RESTOS.indexOf(RESTOS[r])) {
-        alert("La letra DNI "+dni+" tiene la letra.");
-    }
-}
+let dni="2759617";
+alert("El DNI "+dni+" tiene la letra: "+RESTOS[parseInt(dni)%23])
