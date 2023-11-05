34c8bf4bddccc2ebf7e99cb558311506009a5369
Fraile2 <145047146+Fraile2@users.noreply.github.com>
2023-11-05 12:06:20 +0100

    ej9 hecho

--- Ejercicios/Tema4/Ej9/js/js.js
+++ Ejercicios/Tema4/Ej9/js/js.js
@@ -1,7 +1,11 @@
+es_mayusculas_o_minusculas("hola mundo");
+
 function es_mayusculas_o_minusculas(cadena) {
     if (cadena==cadena.toLowerCase()) {
-        
+        console.log("Esta cadena esta en minúsculas");
+    } else if (cadena==cadena.toUpperCase()) {
+        console.log("Esta cadena esta en mayusculas");
     } else {
-        
+        console.log("No se pudo determinar si la cadena está en mayusculas o minusculas");
     }
 }
