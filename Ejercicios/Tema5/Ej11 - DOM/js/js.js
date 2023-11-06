bf8ec400a97f7bba822bbc6541f310fc1cee5da2
Fraile2 <alejandrofraile421@gmail.com>
2023-11-06 10:48:49 +0100

    Ej11 - incompleto

---
+++ Ejercicios/Tema5/Ej11 - DOM/js/js.js
@@ -0,0 +1,4 @@
+let enlaces = document.getElementsByTagName("a");
+console.log("Numero de enlaces: "+enlaces.length);
+// let enlace_penultimo = enlaces[enlaces.length-1];
+// console.log("El penultimo enlace lleva a: "+enlace_penultimo.href);
