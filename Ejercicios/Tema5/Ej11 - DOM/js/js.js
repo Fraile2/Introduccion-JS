f185c83d2b49f096c0e8d15306e9cd89853bb759
Fraile2 <alejandrofraile421@gmail.com>
2023-11-10 09:37:04 +0100

    aaa

--- Ejercicios/Tema5/Ej11 - DOM/js/js.js
+++ Ejercicios/Tema5/Ej11 - DOM/js/js.js
@@ -1,2 +1,4 @@
 let enlaces = document.getElementsByTagName("a");
 console.log("Numero de enlaces: "+enlaces.length);
+console.log("Penúltimo enlace: "+enlaces[length-1]);
+console.log(" enlace: "+enlaces.length);
