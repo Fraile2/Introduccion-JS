83fc11c0b083534b562714e5ecca6be929b88ed8
Fraile2 <alejandrofraile421@gmail.com>
2023-11-10 08:36:48 +0100

    Primer ejercicio Tema 5

--- Ejercicios/Tema5/Ej11 - DOM/js/js.js
+++ Ejercicios/Tema5/Ej11 - DOM/js/js.js
@@ -1,4 +1,2 @@
 let enlaces = document.getElementsByTagName("a");
 console.log("Numero de enlaces: "+enlaces.length);
-// let enlace_penultimo = enlaces[enlaces.length-1];
-// console.log("El penultimo enlace lleva a: "+enlace_penultimo.href);
