79b256c3878fb729204e97c254ca39ff0c45462a
Fraile2 <145047146+Fraile2@users.noreply.github.com>
2023-12-10 11:55:46 +0100

    aa

---
+++ Ejercicios/Tema5/0_Cajon_arena/js/js.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", carga);
+
+function carga(event) {
+    
+}
+
+// function crearElemento(event) {
+//     var parrafo = document.createElement('p');
+//     var contenido = document.createTextNode("¡Hola mundo!");
+//     parrafo.appendChild(contenido);
+//     document.body.appendChild(parrafo);
+// }
