c48a54c500258fed8b206df72424cf55c5fb8ed6
Fraile2 <alejandrofraile420@gmail.com>
2024-01-19 09:29:46 +0100

    aaa

--- Extra/js/js.js
+++ Extra/js/js.js
@@ -1,5 +1,11 @@
-document.addEventListener("keypress", carga);
+document.addEventListener("DOMContentLoaded", carga);
 
 function carga(event) {
-    document.onkeydown("x")
+    formulario.addEventListener('submit', show_nombre);
+}
+
+function show_nombre(event) {
+    event.preventDefault();
+    var nombre = document.getElementById('nombre').value;
+    resultado.innerHTML="Tu nombre es: "+nombre;
 }
