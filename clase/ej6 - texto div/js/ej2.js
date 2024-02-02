f2284a7e9cafd5698633438fa4d7deeb34d7a769
Fraile2 <alejandrofraile420@gmail.com>
2024-02-02 10:15:34 +0100

    js

---
+++ clase/ej6 - texto div/js/ej2.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", carga);
+
+
+function carga(event) {
+    formulario.addEventListener("click", function (event) {
+        event.preventDefault();
+        
+    });
+}
