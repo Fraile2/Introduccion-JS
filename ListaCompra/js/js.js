0d6da2009732258d41de0b8738893bad81e404b4
Fraile2 <alejandrofraile420@gmail.com>
2024-02-05 12:32:47 +0100

    aa

--- ListaCompra/js/js.js
+++ ListaCompra/js/js.js
@@ -1,5 +1,12 @@
 document.addEventListener("DOMContentLoaded", carga)
 
 function carga(event) {
-    
+    addObjeto.addEventListener("click", click_addObjeto);
+    volver.addEventListener("click", click_addObjeto)
+}
+
+function click_addObjeto(event) {
+    event.preventDefault();
+    let formulario1=document.getElementById('formulario1').classList
+    formulario1.toggle("oculto");
 }
