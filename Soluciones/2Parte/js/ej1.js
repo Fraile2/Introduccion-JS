e235dc6a7d1adfd599a88aee41b9442d97688af5
Fraile2 <alejandrofraile420@gmail.com>
2024-02-12 12:21:03 +0100

    Segunda parte

---
+++ Soluciones/2Parte/js/ej1.js
@@ -0,0 +1,22 @@
+function validar(event) {
+    let todo_correcto=true;
+
+
+    if ((nombre.value=="" || nombre.value.length<2) && (direccion.value=="" || (direccion.value).length<10)) {
+        todo_correcto=false;
+    }
+    if (parseInt(edad.value)<18) {
+        todo_correcto=false;
+    }
+    if (passw1.value==passw2.value && (passw1.value).length<=8) {
+        todo_correcto=false;
+    }
+    if (!acepto.checked) {
+        todo_correcto=false;
+    }
+
+    if (todo_correcto==false) {
+        error.style.display="block";
+    }
+    return todo_correcto;
+}
