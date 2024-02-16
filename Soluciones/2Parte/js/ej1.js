4f2045dc0eb1444731a256760385cccfb2a05190
Fraile2 <alejandrofraile420@gmail.com>
2024-02-16 09:56:01 +0100

    aa

--- Soluciones/2Parte/js/ej1.js
+++ Soluciones/2Parte/js/ej1.js
@@ -1,7 +1,7 @@
 function validar(event) {
     let todo_correcto=true;
 
-
+    // Soy yo: Alejandro Fraile del Olmo
     if ((nombre.value=="" || nombre.value.length<2) && (direccion.value=="" || (direccion.value).length<10)) {
         todo_correcto=false;
     }
@@ -11,7 +11,7 @@ function validar(event) {
     if (passw1.value==passw2.value && (passw1.value).length<=8) {
         todo_correcto=false;
     }
-    if (!acepto.checked) {
+    if (!acepto.checked) { // ! -> distinto
         todo_correcto=false;
     }
 
